1ae470ddee242e3360bf0fdc88adf5a11262cee8
zerozeynep <zeynep-demirtas@windowslive.com>
2020-03-13 15:39:53 +0200

    Done.

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,7 +1,6 @@
 import React from 'react';
  
-// Here we add `match` to the arguments so we can access the path information 
-// in `routerProps` that is passed from MoviesPage.js 
+
 const MovieShow = ({match, movies}) => {
   return (
     <div>
