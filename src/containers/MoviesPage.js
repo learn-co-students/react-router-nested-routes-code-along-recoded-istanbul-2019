4a4797b49e262a9f97ae14cdd55768a898751348
yamoonz <yamooonz@gmail.com>
2020-06-03 19:24:51 +0300

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,12 @@
-// .src/containers/MoviesPage.js
 import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
-
-const MoviesPage = ({ match, movies }) => (
-  <div>
-    <MoviesList movies={movies} />
-  </div>
-)
-
-export default MoviesPage
+ 
+const MovieShow = props => {
+ 
+  return (
+    <div>
+      <h3>Movies Show Component!</h3>
+    </div>
+  );
+}
+ 
+export default MovieShow;
